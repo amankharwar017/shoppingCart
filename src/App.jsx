3da0a44d760a3bb9215f214be25d9a253273a410
amankharwar017 <amankharwar755@gmail.com>
2025-08-30 13:35:54 +0530

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import Products from "./components/Products";
 import CartProvider from "./contexts/CartProvider";
+import Header from "./components/Header";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 function App() {
   return (
-    <>
-      <CartProvider>
-        <Products />
-      </CartProvider>
-    </>
+    <CartProvider>
+      <ToastContainer
+        position="bottom-right"
+        newestOnTop={true}
+        autoClose={600}
+        hideProgressBar={true}
+      />
+      <Header />
+      <Products />
+    </CartProvider>
   );
 }
 
--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import { useCart } from "../contexts/CartProvider";
+import styles from "./Product.module.css";
+import { toast } from "react-toastify";
 function Product({ id, title, price, img }) {
-  const { addItemToCart } = useCart();
+  const { addItemToCart, cart } = useCart();
   function handleAdd() {
+    for (let item of cart) {
+      if (item.id === id) {
+        toast.error("Item already added to cart");
+        return;
+      }
+    }
+
     const newCartItem = {
       id: id,
       price: price,
@@ -11,20 +20,17 @@ function Product({ id, title, price, img }) {
       quantity: 1,
     };
     addItemToCart(newCartItem);
+    toast.info("Item added!!");
   }
   return (
-    <div
-      style={{
-        margin: "1rem",
-        padding: "1rem",
-        border: "2px solid #343434",
-      }}
-    >
-      <p>id:{id}</p>
-      <img src={img} alt={title} height={200} />
-      <p>title: {title}</p>
-      <p>price:{price}</p>
-      <button onClick={handleAdd}>Add to cart</button>
+    <div className={styles.product}>
+      <img src={img} alt={title} className={styles.productImage} />
+      <p className={styles.title}>{title}</p>
+      <p className={styles.price}>&#8377;{price}</p>
+
+      <button onClick={handleAdd} className={styles.addToCartBtn}>
+        Add to Cart
+      </button>
     </div>
   );
 }
